a21da0809977435ae6864fb597e75922ba689a93
chen <1120366460@qq.com>
2022-09-28 22:36:02 +0800

    week 2

---
+++ lab2/DebugPractice/DebugExercise3.java
@@ -0,0 +1,30 @@
+import java.io.File;
+
+/**
+ * Created by jug on 1/22/18.
+ */
+public class DebugExercise3 {
+    public static int countTurnips(In in) {
+        int totalTurnips = 0;
+        while (!in.isEmpty()) {
+            String vendor = in.readString();
+            String foodType = in.readString();
+            double cost = in.readDouble();
+            int numAvailable = in.readInt();
+            if (foodType.equals("turnip")) {
+                int newTotal = totalTurnips + numAvailable;
+                totalTurnips = newTotal;
+            }
+            in.readLine();
+        }
+        return totalTurnips;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("./DebugPractice/foods.csv");
+        File directory = new File(".");
+        System.out.println(directory.getAbsolutePath());
+        System.out.println(directory);
+        System.out.println(countTurnips(in));
+    }
+}
